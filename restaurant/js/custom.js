292e759915ce583fc852c4c3146994495ddce6e0
moahmed--attia <m.attia@outlook.sa>
2018-10-27 18:01:06 +0200

    upload restaurant template

---
+++ restaurant/js/custom.js
@@ -0,0 +1,15 @@
+/* global $, Alert, console*/
+
+$(function ()
+{
+    'use strict';
+  
+    //nice scrol
+    $('html').niceScroll({
+        
+       cursorcolor: '#343030',
+        cursorwidth: '15px'
+        
+    });
+    
+});
